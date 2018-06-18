63970780d3d2a06a080a34b78b7a18c8d68060d9
QRaimbault <quentin.raimbault@hetic.net>
2018-06-19 00:46:50 +0200

    🐳 Work on logstash & kibana

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -11,4 +11,7 @@ return [
     CreamIO\BaseBundle\CreamIOBaseBundle::class => ['all' => true],
     CreamIO\UserBundle\CreamIOUserBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
