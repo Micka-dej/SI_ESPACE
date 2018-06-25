94a8029d68efe9417615ba01c2fe3c731201ecb9
Quentin Raimbault <quentin.raimbault@hetic.net>
2018-06-25 16:42:55 +0200

    ✨ Field validation on register form component

--- frontend-code/src/views/Login.vue
+++ frontend-code/src/views/Login.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="login-page">
-    <div class="loginprogress" v-if="isLoading"></div>
+    <div class="login-page-loader" v-if="isLoading">
+      
+    </div>
     <form v-on:submit.prevent="onSubmit">
       <input type="text" v-model="form.username" placeholder="Entrez votre nom d'utilisateur">
-      <input type="text" v-model="form.password" placeholder="Entrez votre mot de passe">
+      <input type="password" v-model="form.password" placeholder="Entrez votre mot de passe">
 
       <button type="submit">Se connecter</button>
     </form>
@@ -42,22 +44,19 @@ export default {
         .then(response => {
           if (response.status === 401) {
             DataStore.isLoggedIn = false;
-            this.isLoading = false;
             alert("Bad credentials");
           } else if (response.status === 200) {
             DataStore.isLoggedIn = true;
           } else {
             console.error("error");
           }
+          this.isLoading = false;
         })
         .catch(error => {
           console.error(error);
           this.isLoading = false;
         });
     }
-  },
-  created() {
-    console.log(DataStore.userDetails);
   }
 };
 </script>
