fd517fe480f335801e4c83a4c31ecee928ece5ca
Quentin Raimbault <quentin.raimbault@hetic.net>
2018-06-25 15:26:49 +0200

    ✨ Improve login system

--- frontend-code/src/views/Login.vue
+++ frontend-code/src/views/Login.vue
@@ -1,30 +1,49 @@
 <template>
   <div class="login-page">
-    <input type="text" placeholder="Entrez votre nom d'utilisateur">
-    <input type="text" placeholder="Entrez votre mot de passe">
-    <button disabled="disabled">Se connecter</button>
+    <form v-on:submit.prevent="onSubmit">
+      <input type="text" v-model="form.username" placeholder="Entrez votre nom d'utilisateur">
+      <input type="text" v-model="form.password" placeholder="Entrez votre mot de passe">
+
+      <button type="submit">Se connecter</button>
+    </form>
     <a v-on:click="redirectToRegister">Créer un compte utilisateur</a>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 import router from "../router/index.js";
 import DataStore from "../datastore/index.js";
 
 import "@ViewStyle/Login.scss";
+import Axios from "axios";
 
 export default {
   name: "Login",
   components: {},
   data() {
     return {
-      title: "Index page !",
-      articles: null
+      form: {
+        username: "",
+        password: ""
+      }
     };
   },
   methods: {
     redirectToRegister: function() {
       router.push("register");
+    },
+    onSubmit() {
+      axios
+        .post("http://localhost:12000/api/login", this.form)
+        .then(response => {
+          console.log(response.data);
+          console.log(response.status);
+        })
+        .catch(error => {
+          console.error(error);
+        });
     }
   },
   created() {
@@ -32,5 +51,3 @@ export default {
   }
 };
 </script>
-
-<style></style>
