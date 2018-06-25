e1d6213495190429e0e58bc288daae45d5f242f2
Chrystelle Poulet <chrystelle.poulet@hetic.net>
2018-06-26 00:56:35 +0200

    💄 Register page UI
     #26

--- frontend-code/src/views/Register.vue
+++ frontend-code/src/views/Register.vue
@@ -28,22 +28,52 @@ export default {
       userDetails: {},
       actualStep: {},
       stepsData: {
+        username: {
+          placeholder: "Votre nom d'utilisateur",
+          title: "Choissisez un nom d'utilisateur :",
+          fname: "username",
+          nextStep: "email"
+        },
         email: {
           placeholder: "Entrez votre e-mail",
-          title: "Adresse e-mail",
+          title: "Veuillez entrer une adresse e-mail :",
           fname: "email",
+          nextStep: "password"
+        },
+        password: {
+          placeholder: "Votre mot de passe",
+          title: "Veuillez choisir un mot de passe :",
+          fname: "password",
+          nextStep: "lastname"
+        },
+        lastname: {
+          placeholder: "Entrez votre nom",
+          title: "Veuillez renseigner votre nom :",
+          fname: "lastname",
           nextStep: "firstname"
         },
         firstname: {
           placeholder: "Entrez votre prénom",
-          title: "Prénom",
+          title: "Veuillez renseigner votre prénom :",
           fname: "firstname",
-          nextStep: "lastname"
+          nextStep: "phoneNumber"
         },
-        lastname: {
-          placeholder: "Entez votre nom de famille",
-          title: "Nom",
-          fname: "lastname",
+        phoneNumber: {
+          placeholder: "Entrez votre numéro",
+          title: "Veuillez renseigner un numéro de téléphone :",
+          fname: "phoneNumber",
+          nextStep: "planet"
+        },
+          planet: {
+          placeholder: "Entrez le nom de votre planète",
+          title: "De quelle planète êtes-vous originaire ?",
+          fname: "planet",
+          nextStep: "credits"
+        },
+        credits: {
+          placeholder: "Entrez le montant de vos crédits",
+          title: "De combien souhaitez-vous créditer votre compte ?",
+          fname: "credits",
           nextStep: ""
         }
       }
