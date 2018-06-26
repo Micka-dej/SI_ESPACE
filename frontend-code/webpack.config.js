fabfa521adcf4c138fd4e702b953e284110dce4e
QRaimbault <quentin.raimbault@hetic.net>
2018-06-26 14:52:15 +0200

    🐛 Fix fonts

--- frontend-code/webpack.config.js
+++ frontend-code/webpack.config.js
@@ -34,6 +34,26 @@ const config = {
       {
         test: /\.scss$/,
         use: ["vue-style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: "images/[hash]-[name].[ext]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.ttf$/,
+        loader: "url-loader",
+        options: {
+          limit: 100000, // Convert images < 8kb to base64 strings
+          name: "fonts/[hash]-[name].[ext]"
+        }
       }
     ]
   },
